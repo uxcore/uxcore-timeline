e4b063bf589df9cf26c1cea914b057ab6a9d1b6b
yize.shc <hanchao.shc@alibaba-inc.com>
2018-01-07 20:04:16 +0800

    feat: add tests

--- tests/Timeline.spec.js
+++ tests/Timeline.spec.js
@@ -1,9 +1,33 @@
 import expect from 'expect.js';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils, { Simulate } from 'react-addons-test-utils';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import Icon from 'uxcore-icon';
 import Timeline from '../src';
 
-describe('Timeline', () => {
+Enzyme.configure({ adapter: new Adapter() });
 
+describe('Timeline', () => {
+  it('should render correctly', () => {
+    mount(<Timeline />);
+  });
+  it('props', () => {
+    const wrapper = mount(<Timeline pending={<a>To do</a>}>
+      <Timeline.Item title="2017-10-25 星期一" dotted>
+        <p>content1</p>
+        <p>content1</p>
+      </Timeline.Item>
+      <Timeline.Item title="2017-10-26 星期二" dot={<Icon name="shezhi" />}>
+        <p>content2</p>
+        <p>content2</p>
+      </Timeline.Item>
+      <Timeline.Item title="2017-10-27 星期三">
+        <p>content3</p>
+        <p>content3</p>
+      </Timeline.Item>
+    </Timeline>);
+    expect(wrapper.find('.uxicon-shezhi')).to.have.length(1);
+    expect(wrapper.find(Timeline)).to.have.length(1);
+    expect(wrapper.contains(<a>To do</a>)).to.equal(true);
+  });
 });
